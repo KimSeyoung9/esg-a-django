bad3c48d528c7a79a6d232a67a0ea765af3c43b3
Kimseyoung9 <kimsey99@korea.ac.kr>
2022-10-26 16:34:50 +0900

    added blog.single_post_page

--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,11 @@ def index(request):
     return render(request,'blog/index.html', {
         "post_list": post_qs,
     })
+
+def single_post_page(request, pk):
+    # 한개만 가져오려고 쓰는 함수 get
+    # pk를 인자로 받아서 키워드 pk가 인자로 받은 pk가 동일하면 함수 수행
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/single_post_page.html', {
+        'post' : post
+    })
