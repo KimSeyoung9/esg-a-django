518374f2f5617578cd91de16c9393847862a9f51
Kimseyoung9 <kimsey99@korea.ac.kr>
2022-10-26 15:12:20 +0900

    Added updated_at to blog.Post

--- blog/models.py
+++ blog/models.py
@@ -5,5 +5,9 @@ class Post(models.Model):
     title = models.CharField(max_length=30) # 길이 제한이 있는 문자열 (max_length가 30)
     content = models.TextField() # 길이 제한이 없는 문자열
 
-    created_at = models.DateTimeField() # 생성된 날짜와 시각을 저장
+    created_at = models.DateTimeField(auto_now_add=True) # 생성된 날짜와 시각을 저장
+    updated_at = models.DateTimeField(auto_now=True)
     # author : 추후 작성 예정
+
+    def __str__(self):
+        return f'[{self.pk}] {self.title}'
